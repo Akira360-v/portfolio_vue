d3a29bbef047663a745ea5b311c05237c79b31f5
Akira360 <akira360.v@gmail.com>
2020-02-13 10:02:47 +0300

    rt-1

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="container">
+      <div class="nav">
+        <div class="nav__logo">
+          <img src="@/assets/images/layouts/main/logo.png" alt="logo" />
+        </div>
+        <div class="nav__link">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
