66c276cb0189ad778ccd486e7a9ff7105e37f758
Akira360 <akira360.v@gmail.com>
2020-02-13 16:30:29 +0300

    rtcolbec

---
+++ src/filters/localize.filter.js
@@ -0,0 +1,22 @@
+// // import store from '../store'
+
+// import ru from '../locales/ru.json'
+// import en from '../locales/en.json'
+
+// // Console.log(123123)
+// // export default function localizeFilter(key) {
+// export default function localizeFilter {
+//   //   // const locale = 
+//   //   // const locale = "ru-RU"
+//   //   // const locale = store.getters || 'ru-RU'
+//   //   // const locale = store.getters
+//   console.log(123);
+
+//   const locales = {
+//     'ru-RU': ru,
+//     'en-US': en
+//   },
+//   locales
+
+//   //   // return locales = store.getters || 'ru-RU'
+// }
---
+++ src/layouts/RegisterLayout.vue
@@ -0,0 +1,21 @@
+// <template>
+//   <div>
+//     <Loader v-if="loading" />
+//     <div class="app-register-layout" v-else>
+//       <div class="qwe">asdasda</div>
+//       <div class="qwe">asdasda</div>
+//       <div class="qwe">asdasda</div>
+//       <div class="qwe">asdasda</div>
+//       <div class="qwe">asdasda</div>
+//       <router-view />
+
+//     </div>
+//   </div>
+// </template>
+
+// <script>
+// export default {}
+// </script>
+
+// <style>
+// </style>
