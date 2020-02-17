acc5efe15dac4ea8d642e359d381e61018c95a97
Akira360 <akira360.v@gmail.com>
2020-02-17 17:59:40 +0300

    rt05 nemu

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -3,9 +3,9 @@
     <div class="container">
       <div class="nav">
         <div class="nav__logo">
-          <figure>
+          <router-link to="/">
             <img src="@/assets/images/layout/main/logo.png" alt="logo" />
-          </figure>
+          </router-link>
         </div>
         <div class="nav__block">
           <div class="nav__link">
@@ -15,10 +15,28 @@
             <router-link to="/contacts">{{$t('navContacts')}}</router-link>
           </div>
           <div class="nav__flag">
-            <a href="#" @click="setLocale('ru')"><flag iso="ru"></flag></a>
-            <a href="#" @click="setLocale('en')"><flag iso="us"></flag></a>
+            <a href="#" @click="setLocale('ru')">
+              <flag iso="ru"></flag>
+            </a>
+            <a href="#" @click="setLocale('en')">
+              <flag iso="us"></flag>
+            </a>
           </div>
         </div>
+
+        <div class="modil__menu">
+          <div class="fade_menu" @click="show = !show" v-bind:class="{ fade_clear: show }">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+
+        <transition name="fade">
+          <ul v-if="show">
+            <li>qweqwe</li>
+          </ul>
+        </transition>
       </div>
     </div>
   </div>
@@ -26,19 +44,20 @@
 
 <script>
 export default {
-  name: 'Navbar',
+  // name: 'Navbar',
+  data: () => ({
+    show: false
+  }),
   methods: {
-    setLocale(locale){
-      import(`../../locales/${locale}.json`).then((msgs) => {
+    setLocale(locale) {
+      import(`../../locales/${locale}.json`).then(msgs => {
         this.$i18n.setLocaleMessage(locale, msgs)
         this.$i18n.locale = locale
       })
     }
   }
-
 }
 </script>
 
 <style>
-
 </style>
